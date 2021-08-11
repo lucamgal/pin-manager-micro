54540a76ec3fa8c050f1072022e1a21ea8aed67b
gallol <luca.gallo@prometeia.com>
2021-08-11 12:06:13 +0200

    GlobalExceptionHandler typo

--- pin-manager-rest/src/main/java/it/gallo/pinmanager/rest/GlobalExceptionHandler.java
+++ pin-manager-rest/src/main/java/it/gallo/pinmanager/rest/GlobalExceptionHandler.java
@@ -29,19 +29,19 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(NotFoundPinVerificationException.class)
 	@ResponseBody ApiError handleNotFoundPinVerificationException(HttpServletRequest req, Exception ex) {
-		return new ApiError(ex.getLocalizedMessage(), req.getRequestURL().toString(), Instant.now(), HttpStatus.BAD_REQUEST.value());
+		return new ApiError(ex.getLocalizedMessage(), req.getRequestURL().toString(), Instant.now(), HttpStatus.NOT_FOUND.value());
 	}
 
 	@ResponseStatus(HttpStatus.FORBIDDEN)
 	@ExceptionHandler(NotPermittedPinVerificationException.class)
 	@ResponseBody ApiError handleNotPermittedPinVerificationException(HttpServletRequest req, Exception ex) {
-		return new ApiError(ex.getLocalizedMessage(), req.getRequestURL().toString(), Instant.now(), HttpStatus.BAD_REQUEST.value());
+		return new ApiError(ex.getLocalizedMessage(), req.getRequestURL().toString(), Instant.now(), HttpStatus.FORBIDDEN.value());
 	}
 
 	@ResponseStatus(HttpStatus.FORBIDDEN)
 	@ExceptionHandler(TooManyPinVerificationException.class)
 	@ResponseBody ApiError handleTooManyPinVerificationException(HttpServletRequest req, Exception ex) {
-		return new ApiError(ex.getLocalizedMessage(), req.getRequestURL().toString(), Instant.now(), HttpStatus.BAD_REQUEST.value());
+		return new ApiError(ex.getLocalizedMessage(), req.getRequestURL().toString(), Instant.now(), HttpStatus.FORBIDDEN.value());
 	}
 
 }
